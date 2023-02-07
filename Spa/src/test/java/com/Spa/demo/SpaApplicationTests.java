3eeb28cf6c2f60dede586a732f9934b5ec39eabf
YurvikP <91148042+YurvikP@users.noreply.github.com>
2023-02-06 18:12:27 -0600

    Add files via upload

---
+++ Spa/src/test/java/com/Spa/demo/SpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spa.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
